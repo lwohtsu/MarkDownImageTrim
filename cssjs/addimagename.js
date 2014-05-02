0e1664b43ec90d589acb8d382c123236160193f4
Ohtsu Yuichiro <ohtsu-y1@libroworks.co.jp>
2014-05-03 01:57:41 +0900

    frist commit

---
+++ cssjs/addimagename.js
@@ -0,0 +1,57 @@
+$(function(){
+	$('img').each(function(){
+		var t = $(this);
+		t.after('<p class="caption">▲：'+ t.attr('alt') + '</p>');
+		var s = t.attr('src');
+		if(s.indexOf('?')>=0){
+			//トリミング用クラス
+			t.before('<p>（'+ s.substring(0, s.lastIndexOf('?')) + '）</p>');
+			if(s.indexOf('?clip=')>=0){
+				imageClipping(t, s.substr(s.indexOf('=')+1));
+			}
+		} else {
+			t.before('<p>（'+ s.substr(s.lastIndexOf('/')+1) + '）</p>');
+		}
+	});
+	$('code').each(function(){
+		var c = $(this).attr('class');
+		var p = $(this).parent('pre');
+		p.addClass('prettyprint');
+		p.addClass(c);
+		//p.addClass('linenums')
+	});
+});
+
+function imageClipping(targ, qry){
+	var param = qry.split('+');
+	var repelem = $('<div class="trimbase"></div>');
+	var maxwidth = parseFloat(targ.css('max-width'));
+	var contwidth = targ[0].naturalWidth;
+	var zoom = 1;
+	var piczoom = 1;
+	if(parseFloat(param[2]) > maxwidth){
+		// console.log(contwidth);
+		// console.log(maxwidth);
+		// console.log(param[2]);
+		//トリミング対象の幅がmax-widthを超える
+		zoom = maxwidth / param[2];
+		// console.log(zoom);
+		piczoom = (contwidth*zoom) / maxwidth;
+		param[0] *= zoom;
+		param[1] *= zoom;
+		param[2] *= zoom;
+		param[3] *= zoom;
+	} else {
+		//トリミング対象の幅がmax-width未満
+		piczoom = contwidth / param[2];
+	}
+
+	repelem.css({
+		backgroundImage: 'url(' + targ.attr('src') +')',
+		'background-position': -param[0] + 'px ' + -param[1] + 'px',
+		'background-size': piczoom * 100 + '%', 
+		width: param[2], height: param[3],
+		'background-repeat': 'no-repeat'
+	});
+	targ.replaceWith(repelem);
+}
---
+++ text/vc2013chap3.md
@@ -0,0 +1,18 @@
+<script src="../cssjs/jquery-2.0.3.min.js"></script>
+<script src="../cssjs/addimagename.js"></script>
+<link rel="stylesheet" href="../cssjs/myrule.css"> 
+
+まずウィンドウアプリケーション用のプロジェクトを作成します。〈新しいプロジェクト〉ボタンをクリックして、「chap3」という名前のプロジェクトを作成しましょう。この名前は好みで変更してかまいません。
+
+>ツールボタンが見えるよう。下はカット
+
+![キャプションなし](img/c03-01-06.png?clip=0+0+600+300)
+<div class="zuchuu">〈新しいプロジェクト〉をクリック</div>
+
+
+
+
+
+
+
+
